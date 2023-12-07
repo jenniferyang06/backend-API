43763c2e8dd4655801d4a967e47ed50a9e17e8ed
Your Name <you@example.com>
2023-12-07 21:23:34 +0800

    Added UserRequest and Show and Store API

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,8 +3,11 @@
 namespace App\Http\Controllers\Api;
 
 use App\Models\User;
-use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Http\Requests\UserRequest;
+use App\Http\Controllers\Controller;
+use Illuminate\Support\Facades\Hash;
+
 
 class UserController extends Controller
 {
@@ -22,9 +25,16 @@ class UserController extends Controller
     /**
      * Store a newly created resource in storage.
      */
-    public function store(Request $request)
+    public function store(UserRequest $request)
     {
         //
+        $validated = $request->validated();
+
+        $validated['password'] = Hash::make($validated['password']);
+
+        $user = User::create($validated);
+
+        return $user;
     }
 
     /**
@@ -33,6 +43,7 @@ class UserController extends Controller
     public function show(string $id)
     {
         //
+        return User::findOrFail($id);
     }
 
    
